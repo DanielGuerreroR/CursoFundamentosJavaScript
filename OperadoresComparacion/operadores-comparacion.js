7fe4d12fed1633fd74bd8cd295b50e9aea7734ef
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-07 11:34:07 -0500

    Clase en la que se vio los operadores de comparacion que comunmente se usan en funciones

---
+++ OperadoresComparacion/operadores-comparacion.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log(a == b);
+
+console.log(a === c);
+
+console.log(a != b);
+
+console.log(a !== c);
+
+console.log(a > b);
+
+console.log(a <= b);
+
+console.log(a > c);
