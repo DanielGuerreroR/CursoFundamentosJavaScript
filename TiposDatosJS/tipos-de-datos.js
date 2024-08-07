f88f847572b3a3908a5159177c80a64b2fce6b44
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-07 09:52:42 -0500

    clase en la que se vio todos los tipos de datos, primitivos y complejos y el como se declaran

---
+++ TiposDatosJS/tipos-de-datos.js
@@ -0,0 +1,36 @@
+// PRIMITIVOS
+
+//Strings
+let nombre = "Tere";
+
+//number
+let edad = 25;
+
+//boolean
+let esMayorDeEdad = true;
+
+//null
+let noHayValor = null;
+
+//undefined
+let noDefinido = undefined;
+
+//symbol
+let simboloUnico = Symbol("unico");
+
+//bigint
+let numeroGrande = 2n;
+
+// COMPLEJOS
+
+// object
+let carro = {
+	marca: "Tesla",
+	modelo: "Model S",
+};
+
+// 'frutas'
+let frutas = ["manzana", "banano", "uvas"];
+
+// function
+function saludar() {}
