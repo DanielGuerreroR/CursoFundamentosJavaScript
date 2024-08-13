014425c352d13522c06fa299e1fb07b032b89e4a
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-13 10:53:23 -0500

    Clase donde se vio la estructura condicional if

---
+++ EjecucionCondicionalIf/condicional-if.js
@@ -0,0 +1,39 @@
+// // Primera estructura de if'
+// if (let === algo){
+// 	codigo
+// } else {
+
+// }
+
+// // else if'else if'
+// if (let === algo){
+// 	codigo
+// } else if () {
+
+// }
+
+// ejemplo1
+let nombre = "Diego";
+//Si el nombre es igual a diego pasara algo si no no pasara nada
+if (nombre === "Diego") {
+	console.log("Hola Diego");
+}
+
+// // ejemplo2
+let nombre2 = "Nico";
+if (nombre2 === "Diego") {
+	console.log("Hola Diego");
+} else {
+	console.log("Nombre no encontrado");
+}
+
+// ejemplo3
+let nombre3 = "Nico";
+//Si el nombre es igual a diego pasara algo si no no pasara nada
+if (nombre3 === "Diego") {
+	console.log("Hola Diego");
+} else if (nombre3 === "Nico") {
+	console.log("Hola Nico");
+} else {
+	console.log("Nombre no encontrado");
+}
