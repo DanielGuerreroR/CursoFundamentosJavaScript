87b8c9cc5911f50ecd5b3073ece28640f598d03c
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-14 10:57:23 -0500

    clase en la que se empezo con los loops inciando con for

---
+++ LoopFor/loop-for.js
@@ -0,0 +1,11 @@
+// // Estructura
+// for (variable;condicion;incremento){
+// 	codigo a ejecutar
+// }
+
+// ejemplo1
+let list = ["eat", "sleep", "code", "repeat"];
+
+for (let i = 0; i < list.length; i++) {
+	console.log(list[i]);
+}
