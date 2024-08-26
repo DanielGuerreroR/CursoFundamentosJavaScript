5ffcb5e40430a72e2e5d04c01e811e349da52d0f
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-26 15:35:26 -0500

    clase en la que se vio los metodos de push y pop para los array

---
+++ PushPop/push-pop.js
@@ -0,0 +1,12 @@
+//'Utilizando push()'
+const countries = ["USA", "Canada", "UK"];
+const newCountries = countries.push("Germany", "Australia");
+
+console.log(countries);
+console.log(newCountries);
+
+//'Utilizando pop()'
+const removeCountry = countries.pop();
+
+console.log(countries);
+console.log(removeCountry);
