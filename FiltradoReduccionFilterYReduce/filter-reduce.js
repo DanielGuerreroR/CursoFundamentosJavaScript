542aa8aeb72bdaf129c0d3f4bc905ef581e59f82
Daniel Guerrero <dansan_2002@hotmail.com>
2024-08-28 14:39:05 -0500

    clase en la que se vieron los metodos filter y reduce

---
+++ FiltradoReduccionFilterYReduce/filter-reduce.js
@@ -0,0 +1,30 @@
+//'filter()'
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+
+// console.log(numbers);
+// console.log(evenNumbers);
+
+//'reduce()'
+const numbersReduce = [1, 2, 3, 4, 5];
+const sum = numbersReduce.reduce(
+	(accumulator, currentValue) => accumulator + currentValue,
+	0
+);
+
+// console.log(numbersReduce);
+// console.log(sum);
+
+//'reduce() - caso 2'
+const words = ["apple", "hello", "baby", "bye", "baby", "bye", "bye"];
+
+const wordFrecuency = words.reduce((accumulator, currentValue) => {
+	if (accumulator[currentValue]) {
+		accumulator[currentValue]++;
+	} else {
+		accumulator[currentValue] = 1;
+	}
+	return accumulator;
+}, {});
+
+console.log(wordFrecuency);
